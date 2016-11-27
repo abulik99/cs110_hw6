2bdcee13afe476e9bc30cc96381ac899c0269a10
abulik99 <abulik99>
2016-11-27 06:14:59 -0800

    update

---
+++ shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Rectangle 
+{
+	private int width, height;
+	public Rectangle(int width, int height)
+	{
+		this.width=width;
+		this.height=height;
+	}
+	public int getArea()
+	{
+		return this.width*this.height;
+	}
+	public void main(String[] args)
+	{
+		
+	}
+}
---
+++ shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square 
+{
+	private int length;
+	public Square(int len)
+	{
+		this.length=len;
+	}
+	public int getArea()
+	{
+		return length*length;
+	}
+	public static void main(String[] args)
+	{
+		
+	}
+}
